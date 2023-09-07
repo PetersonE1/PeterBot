f0e93b7e55d88402b3339b606c7525f1341b59f3
Erik Alexander Peterson <PetersonE_1@LIVE.COM>
2023-09-07 11:38:47 -0400

    Add project files.

---
+++ PeterBot/Program.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.WebSocket;
+using PeterBot.Services;
+
+namespace PeterBot
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            var token = "NDM3MDgwNjQ0MDgyOTkxMTI0.GHcYm1.ebdQkLhNWJUb1gapRqVDKQw8sNQqi4S7TcnODA";
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            SlashCommands commands = new SlashCommands(_client);
+
+            Console.ReadLine();
+
+            await _client.LogoutAsync();
+            await _client.StopAsync();
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ PeterBot/Services/SlashCommands.cs
@@ -0,0 +1,49 @@
+﻿using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterBot.Services
+{
+    public class SlashCommands
+    {
+        private readonly DiscordSocketClient _client;
+
+        public SlashCommands(DiscordSocketClient client)
+        {
+            _client = client;
+            _client.Ready += Client_Ready;
+            _client.SlashCommandExecuted += SlashCommandHandler;
+        }
+
+        public async Task Client_Ready()
+        {
+            var globalCommand = new SlashCommandBuilder();
+            globalCommand.WithName("first-global-command");
+            globalCommand.WithDescription("Echoes back");
+            globalCommand.AddOption("message", ApplicationCommandOptionType.String, "Message to be echoed", isRequired: true);
+
+            try
+            {
+                await _client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+            }
+            catch (ApplicationCommandException ex)
+            {
+                var json = JsonConvert.SerializeObject(ex.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+            }
+        }
+
+        private async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            var message = command.Data.Options.First().Value as string;
+
+            await command.RespondAsync(message);
+        }
+    }
+}
